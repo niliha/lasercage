e9a9baf0da49a3eddafacc83f57d75009e61349a
niliha <75397148+niliha@users.noreply.github.com>
2023-08-19 11:42:45 +0200

    Fix arduino serial output

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 extern "C" void app_main() {
     // initialize arduino library before we start the tasks
     initArduino();
-
     Serial.begin(115200);
+    sleep(3);
 
     // nvs_flash_erase();  // erase the NVS partition and...
     // nvs_flash_init();   // initialize the NVS partition.
@@ -18,7 +18,14 @@ extern "C" void app_main() {
     }
     // Network::initWifiAccessPoint(WifiCredentials::ssid, WifiCredentials::password);
 
-    MD_MAX72XX max7219(MD_MAX72XX::FC16_HW, 5);
+    while (true) {
+        // Stay in setup() such that stack frame is not popped
+    Serial.printf("Chilling in loop\n...");
+        delay(1000);
+    }
+
+    Serial.printf("Constructing LaserCage...\n");
+    MD_MAX72XX max7219(MD_MAX72XX::FC16_HW,7,6,5,1);
     LaserCage laserCage(max7219, 50);
 
     while (true) {
